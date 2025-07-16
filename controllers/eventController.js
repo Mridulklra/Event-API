b46445f2a0eb80ee43571a0623a73dafe9ebe219
Mridul Kalra <mridulkalra700@gmail.com>
2025-07-16 08:28:31 +0530

    GEt event details with registered users

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -30,4 +30,41 @@ const createEvent = async (req, res) => {
   }
 };
 
-module.exports = { createEvent };
+const getEventDetails = async (req, res) => {
+  const eventId = req.params.id;
+
+  try {
+   const eventQuery = `
+      SELECT * FROM events WHERE id = $1
+    `;
+    const eventResult = await pool.query(eventQuery, [eventId]);
+
+    if (eventResult.rows.length === 0) {
+      return res.status(404).json({ error: 'Event not found' });
+    }
+
+    const event = eventResult.rows[0];
+
+  
+    const userQuery = `
+      SELECT u.id, u.name, u.email
+      FROM users u
+      JOIN registrations r ON u.id = r.user_id
+      WHERE r.event_id = $1
+    `;
+    const userResult = await pool.query(userQuery, [eventId]);
+
+    event.registered_users = userResult.rows;
+
+    res.status(200).json(event);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  createEvent,
+  getEventDetails
+};
+
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {createEvent} = require('../controllers/eventController');
+const {getEventDetails} = require('../controllers/eventController');
  router.post('/',createEvent);
+ router.get('/:id', getEventDetails);
  module.exports = router;
